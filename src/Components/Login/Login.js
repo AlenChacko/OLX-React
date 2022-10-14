beb792ebfb3a1df715610896ffa5c1195d858e12
Alen Chacko <user.email>
2022-10-14 11:17:55 +0530

    final commit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,22 +1,26 @@
-import React,{useState,useContext} from 'react';
-import {FirebaseContext} from '../../store/Context'
-import Logo from '../../olx-logo.png';
-import './Login.css';
-import {useHistory, Link} from 'react-router-dom'
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../../store/Context";
+import Logo from "../../olx-logo.png";
+import "./Login.css";
+import { useHistory, Link } from "react-router-dom";
 
 function Login() {
-const [email,setEmail]=useState('')
-const [password,setPassword]=useState('')
-const {firebase} =useContext(FirebaseContext)
-const history=useHistory()
-const handleLogin=(e)=>{
-  e.preventDefault()
-  firebase.auth().signInWithEmailAndPassword(email,password).then(()=>{
-    history.push('/')
-  }).catch((error)=>{
-    alert(error.message)
-  })
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { firebase } = useContext(FirebaseContext);
+  const history = useHistory();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push("/");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
   return (
     <div>
       <div className="loginParentDiv">
@@ -29,10 +33,11 @@ const handleLogin=(e)=>{
             type="email"
             value={email}
             id="fname"
-            onChange={(e)=> setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             name="email"
             defaultValue="John"
-          required />
+            required
+          />
           <br />
           <label htmlFor="lname">Password</label>
           <br />
@@ -41,15 +46,16 @@ const handleLogin=(e)=>{
             type="password"
             value={password}
             id="lname"
-            onChange={(e)=> setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             name="password"
             defaultValue="Doe"
-          required />
+            required
+          />
           <br />
           <br />
           <button>Login</button>
         </form>
-        <Link to={'/signup'}>Signup</Link>
+        <Link to={"/signup"}>Signup</Link>
       </div>
     </div>
   );
